13597a54da2da7339fee75b35ff80b47f7699c46
Daniel Toledo <toledo992905@gmail.com>
2023-07-25 17:56:20 -0500

    Start App

---
+++ modules/get_input.rb
@@ -0,0 +1,30 @@
+module GetInput
+  def print_options(options)
+    options.each.with_index do |option, index|
+      puts "#{index + 1}. #{option.capitalize}"
+    end
+  end
+
+  def get_with_options(prompt, options)
+    input = ""
+    until options.include?(input.downcase)
+      puts prompt
+      print_options(options)
+      print "> "
+      input = gets.chomp.downcase
+    end
+
+    input
+  end
+
+  def get_input(prompt)
+    input = ""
+    while input.empty?
+      puts prompt
+      print "> "
+      input = gets.chomp
+    end
+
+    input
+  end
+end
